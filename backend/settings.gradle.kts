355d2a2a6a00e35e990afe50d2a8d8238df54f35
Richard Van Camp <richard.vancamp@monad.fi>
2021-09-10 11:47:16 +0300

    Feat: add jib build of docker image

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,7 +1,9 @@
 import de.fayard.refreshVersions.core.FeatureFlag.LIBS
 
 plugins {
-    id("de.fayard.refreshVersions").version("0.10.1")
+    id("de.fayard.refreshVersions").version("0.20.0")
+    // workaround https://github.com/jmfayard/refreshVersions/issues/361
+    id("com.google.cloud.tools.jib").version("3.1.4").apply(false)
 }
 
 refreshVersions {
